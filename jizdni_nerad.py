cc12a6ee759200dd0614246286e7806c82ed2503
Michael Hlaváček <michael.hlavacek@blindspot.ai>
2024-10-25 15:12:57 +0200

    v1.0.0

---
+++ jizdni_nerad.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import pandas as pd
+import requests
+import datetime
+from zoneinfo import ZoneInfo
+from urllib.parse import unquote
+
+st.set_page_config(page_title="Jízdní neřád")
+st.title("Jízdní neřád")
+
+stops = st.text_input(
+    "Zastávky oddělěné středníkem",
+    value="Divadlo Gong;Ocelářská;Českomoravská",
+)
+
+date = st.date_input("Datum odjezdu")
+time = st.time_input("Čas odjezdu")
+
+time_from = datetime.datetime.combine(date, time).replace(tzinfo=ZoneInfo("Europe/Prague"))
+
+raw_data = requests.get(
+    "https://api.golemio.cz/v2/pid/departureboards",
+    params={
+        "names": stops.split(";"),
+        "timeFrom": time_from.isoformat(),
+        "minutesAfter": "30",
+        "limit": "1000",
+    },
+    headers={
+        "X-Access-Token": st.secrets.access_token,
+    }
+).json()
+
+stop_id_to_name = {
+    stop["stop_id"]: unquote(stop["stop_name"])
+    for stop in raw_data["stops"]
+}
+
+departures = pd.DataFrame.from_records(
+    [
+        {
+            "predicted_departure": datetime.datetime.fromisoformat(departure["departure_timestamp"]["predicted"]),
+            "stop": stop_id_to_name[departure["stop"]["id"]],
+            "route": unquote(departure["route"]["short_name"]),
+            "headsign": unquote(departure["trip"]["headsign"]),
+            "scheduled_departure": datetime.datetime.fromisoformat(departure["departure_timestamp"]["scheduled"]),
+            "last_stop": departure["last_stop"]["name"],
+            "platform": unquote(departure["stop"]["platform_code"]),
+        }
+        for departure in raw_data["departures"]
+    ]
+)
+departures["time_until_departure"] = departures["predicted_departure"] - time_from
+
+columns = {
+    "predicted_departure": st.column_config.DatetimeColumn("Čas odjezdu", timezone="Europe/Prague", format="HH:mm:ss"),
+    "time_until_departure": st.column_config.Column("Doba do odjezdu"),
+    "stop": st.column_config.TextColumn("Zastávka"),
+    "route": st.column_config.TextColumn("Linka"),
+    "headsign": st.column_config.TextColumn("Směr"),
+    "scheduled_departure": st.column_config.DatetimeColumn("Čas odjezdu dle jízdního řádu", timezone="Europe/Prague", format="HH:mm:ss"),
+    "last_stop": st.column_config.TextColumn("Naposledy na zastávce"),
+    "platform": st.column_config.TextColumn("Nástupiště"),
+}
+
+st.dataframe(
+    departures,
+    column_config=columns,
+    column_order=list(columns.keys()),
+    hide_index=True,
+)
